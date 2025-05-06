e93e2bbedb68197a29ff55e0ccb5e2fff5c778c7
Natalie Blumen <natalie.blumen@gmail.com>
2025-05-06 17:23:09 -0400

    may6

--- controllers/home.js
+++ controllers/home.js
@@ -8,5 +8,27 @@ module.exports = {
                 console.log(err);
                 res.status(500).send("Server Error");
             }
-        }
+        },
+
+        getSignUpCustomer: async(req, res) => {
+            try {
+                res.render("signup-customer.ejs");
+
+            } catch(err) {
+                console.log(err);
+                res.status(500).send("Server Error");
+            }
+        },
+
+        getSignUpBusiness: async(req, res) => {
+            try {
+                res.render("signup-business.ejs");
+
+            } catch(err) {
+                console.log(err);
+                res.status(500).send("Server Error");
+            }
+        },
+
+
 };
--- routes/main.js
+++ routes/main.js
@@ -3,6 +3,9 @@ const router = express.Router();
 const homeController = require("../controllers/home");
 
 router.get("/", homeController.getIndex);
+router.get("/signup-customer", homeController.getSignUpCustomer);
+router.get("/signup-business", homeController.getSignUpBusiness);
+
 
 
 module.exports = router;
