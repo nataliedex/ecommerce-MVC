1d772eeece971aef6fb7ee655478f0ed770d3566
Natalie Blumen <natalie.blumen@gmail.com>
2025-05-21 15:52:48 -0400

    may21

--- controllers/home.js
+++ controllers/home.js
@@ -1,13 +1,25 @@
 const Product = require("../models/Product");
 const Business = require("../models/Business");
+const Customer = require("../models/Customer");
 
 module.exports = {
 
         getIndex: async(req, res) => {
             try {
+
+                let user = null;
+
+                if(req.user){
+                    user = req.user.type === "business"
+                     ? await Business.find(req.user._id) 
+                     : await Customer.find(req.user._id);
+                }
+                
+
                 const product = await Product.aggregate([{ $sample: { size: 3 } }]);
                 res.render("index.ejs", {
                     product: product,
+                    user: user,
                 });
 
             } catch(err) {
